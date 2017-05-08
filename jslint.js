317db1662de43b27acccd3b9ecc6d1e4b8ba5337
渡邉 徹彦 <tewatana@gmail.com>
2017-05-08 12:54:33 +0900

    Fix indentation

--- jslint.js
+++ jslint.js
@@ -1199,7 +1199,7 @@ var JSLINT = (function () {
             at = source_row.search(/ \t/);
             if (at >= 0) {
                 warn_at('mixed', line, at + 1);
-			}
+            }
             source_row = source_row.replace(/\t/g, tab);
             at = source_row.search(cx);
             if (at >= 0) {
